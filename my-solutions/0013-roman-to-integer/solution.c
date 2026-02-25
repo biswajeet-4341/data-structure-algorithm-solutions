5201e4311a29e23ed142e36b8a8648ab0b176eba
Biswajeet Mohapatra <secondarybiswajeet@gmail.com>
2026-02-25 15:46:16 +0000

    Sync LeetCode submission Runtime - 2 ms (23.39%), Memory - 10.3 MB (100.00%)

---
+++ my-solutions/0013-roman-to-integer/solution.c
@@ -0,0 +1,30 @@
+int value(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s) {
+    int integer = 0, prev = 0;
+
+    for (int i = strlen(s) - 1; i >= 0; i--) {
+        int curr = value(s[i]);
+
+        if (curr < prev) {
+            integer -= curr;
+        } else {
+            integer += curr;
+        }
+
+        prev = curr;
+    }
+
+    return integer;
+}
